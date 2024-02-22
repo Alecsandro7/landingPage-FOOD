376d65306fafd16e9a3a54d8d558faf591f54b30
Alecsandro Sales <alecsandrosales1@gmail.com>
2024-02-22 17:00:09 -0300

    landing Page made - jus a few animations to go

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const menuHamburger = document.getElementById('mobile-btn')
 
 menuHamburger.addEventListener('click', () => {
-
     const i = menuHamburger.children
     i[0].classList.toggle('fa-x')
-
-
     const mobileMenu = document.getElementById('mobile_menu')
-
     mobileMenu.classList.toggle('active')
-
-    menuHamburger.classList.toggle('active')
-
-
-
-
 })
